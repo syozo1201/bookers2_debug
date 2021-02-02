212b198f0e486bb075efcd1c2d3da24204528b72
syozo1201 <00syozo1201@gmail.com>
2021-02-02 07:58:49 +0000

    update

--- bookers2-debug/app/controllers/books_controller.rb
+++ bookers2-debug/app/controllers/books_controller.rb
@@ -1,18 +1,26 @@
 class BooksController < ApplicationController
 
+  before_action :authenticate_user!
+  before_action :ensure_current_user, {only: [:edit,:update,:destroy]}
+
+
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @new_book = Book.new
+
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to books_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
       render 'index'
@@ -34,16 +42,16 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
